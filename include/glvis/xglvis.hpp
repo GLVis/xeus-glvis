a6c286016b4e7f025ca996491519be064b6ab25b
Tom Stitt <tomstitt@hey.com>
2021-08-19 09:38:51 -0700

    move header to install location, rename properties and some methods

--- include/glvis/xglvis.hpp
+++ include/glvis/xglvis.hpp
@@ -1,10 +1,10 @@
 // Copyright(c) 2010 - 2021, Lawrence Livermore National Security, LLC.Produced
 // at the Lawrence Livermore National Laboratory.All Rights reserved.See files
 // LICENSE and NOTICE for details.LLNL - CODE - 443271.
-// 
+//
 // This file is part of the GLVis visualization tool and library.For more
 // information and source code availability see https: // glvis.org.
-// 
+//
 // GLVis is free software; you can redistribute it and / or modify it under the
 // terms of the BSD - 3 license.We welcome feedback and contributions, see file
 // CONTRIBUTING.md for details.
@@ -48,11 +48,11 @@ private:
   static constexpr const char *model_version = "0.1.1";
   static constexpr const char *view_version = "0.1.1";
 
-  XPROPERTY(std::string, D, _data_str);
-  XPROPERTY(std::string, D, _data_type);
-  XPROPERTY(int, D, _width, default_width);
-  XPROPERTY(int, D, _height, default_height);
-  XPROPERTY(bool, D, _is_new_stream, true);
+  XPROPERTY(std::string, D, data_str);
+  XPROPERTY(std::string, D, data_type);
+  XPROPERTY(int, D, width, default_width);
+  XPROPERTY(int, D, height, default_height);
+  XPROPERTY(bool, D, is_new_stream, true);
 
   inline void sync(const std::string &stream, bool is_new = true) {
     auto offset = stream.find('\n');
@@ -60,9 +60,9 @@ private:
       fprintf(stderr, "invalid stream: %s\n", stream.c_str());
       throw std::runtime_error("invalid stream");
     }
-    _data_type = stream.substr(0, offset);
-    _data_str = stream.substr(offset + 1);
-    _is_new_stream = is_new;
+    data_type = stream.substr(0, offset);
+    data_str = stream.substr(offset + 1);
+    is_new_stream = is_new;
   }
 
   inline void set_defaults() {
@@ -84,32 +84,30 @@ public:
                               xeus::buffer_sequence &buffers) const {
     base_type::serialize_state(state, buffers);
 
-    xw::xwidgets_serialize(_data_str(), state["_data_str"], buffers);
-    xw::xwidgets_serialize(_data_type(), state["_data_type"], buffers);
-    xw::xwidgets_serialize(_width(), state["_width"], buffers);
-    xw::xwidgets_serialize(_height(), state["_height"], buffers);
-    xw::xwidgets_serialize(_is_new_stream(), state["_is_new_stream"], buffers);
+    xw::xwidgets_serialize(data_str(), state["data_str"], buffers);
+    xw::xwidgets_serialize(data_type(), state["data_type"], buffers);
+    xw::xwidgets_serialize(width(), state["width"], buffers);
+    xw::xwidgets_serialize(height(), state["height"], buffers);
+    xw::xwidgets_serialize(is_new_stream(), state["is_new_stream"], buffers);
   }
 
   inline void apply_patch(const nl::json &patch,
                           const xeus::buffer_sequence &buffers) {
     base_type::apply_patch(patch, buffers);
 
-    set_property_from_patch(_data_str, patch, buffers);
-    set_property_from_patch(_data_type, patch, buffers);
-    set_property_from_patch(_width, patch, buffers);
-    set_property_from_patch(_height, patch, buffers);
-    set_property_from_patch(_is_new_stream, patch, buffers);
+    set_property_from_patch(data_str, patch, buffers);
+    set_property_from_patch(data_type, patch, buffers);
+    set_property_from_patch(width, patch, buffers);
+    set_property_from_patch(height, patch, buffers);
+    set_property_from_patch(is_new_stream, patch, buffers);
   }
 
-  inline void resize(int width = default_width, int height = default_height) {
-    _width = width;
-    _height = height;
+  inline void set_size(int width = default_width, int height = default_height) {
+    width = width;
+    height = height;
   }
 
-  // `display' conflicts with something in one of the parent classes
-  // or xmaterialize
-  inline void show(const std::string &stream) { sync(stream, true); }
+  inline void draw(const std::string &stream) { sync(stream, true); }
 
   inline void update(const std::string &stream) { sync(stream, false); }
 };
